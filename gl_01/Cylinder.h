2f6d0a3b8fc04cc0858379e990d0aad56ea21f54
Roman Moskalenko <rmoskale@mion.elka.pw.edu.pl>
2019-04-20 17:18:36 +0200

    Rewrite generating vertices and indices, added texture usage

--- gl_01/Cylinder.h
+++ gl_01/Cylinder.h
@@ -7,14 +7,13 @@
 #include "Object.h"
 #include "ShaderProvider.h"
 #include "Shader.h"
+#include "SOIL.h"
 
 /*
 For pi constant
 */
 #define _USE_MATH_DEFINES
 #include <math.h>
-
-#define NULL_VECTOR glm::vec3(0.0f, 0.0f, 0.0f)
 
 /*
 Number of segments which makes cylinder smooth enough
@@ -25,9 +24,8 @@ class Cylinder : Object
 {
 private:
 
-
 	/*
-	Two planes - Top and Bottom, maybe will be changed later if there will be any planes between Top and Bottom
+	Two planes - Top and Bottom, maybe will be changed later if there should be any planes between Top and Bottom
 	*/
 	const GLuint stacks = 2;
 
@@ -54,32 +52,63 @@ private:
 
 	std::vector<GLfloat> vertices;
 	std::vector<GLuint> indices;
-
 	std::shared_ptr<Shader> shader;
 	GLuint VAO, VBO, EBO;
-
+	GLuint texture0;
 
 	void generateVertices()
 	{
-		glm::vec3 centerCoords = coordinates;
-		
-		for (size_t i = 0; i < stacks; i++)
-		{
-			centerCoords.z = (float) (height * i / (stacks - 1));
+		glm::vec3 centerCoords = glm::vec3(0.0f, 0.0f, 0.0f);
+		GLfloat x, y, z = 0.0f, u, v;
 
-			vertices.push_back(centerCoords.x);
-			vertices.push_back(centerCoords.y);
-			vertices.push_back(centerCoords.z);
-				
-			float z = centerCoords.z, x, y;
-			for (size_t j = 0; j < segments; j++)
+		// Bottom plane
+		vertices.push_back(centerCoords.x);
+		vertices.push_back(centerCoords.y);
+		vertices.push_back(centerCoords.z);
+		vertices.push_back(0.25f);
+		vertices.push_back(0.25f);
+		for (size_t i = 0; i < segments; i++)
 			{
-				x = centerCoords.x + radius * glm::cos((float)j / segments * 2 * (float)M_PI);
-				y = centerCoords.y + radius * glm::sin((float)j / segments * 2 * (float)M_PI);
+				u = glm::cos((float)i / segments * 2 * (float)M_PI);
+				v = glm::sin((float)i / segments * 2 * (float)M_PI);
+				x = centerCoords.x + radius * u;
+				y = centerCoords.y + radius * v;
+				u = 0.25f + 0.25f * u;
+				v = 0.25f + 0.25f * v;
 				vertices.push_back(x);
 				vertices.push_back(y);
 				vertices.push_back(z);
+				vertices.push_back(u);
+				vertices.push_back(v);
+		}
+
+		// Top plane (just translated Bottom plane)
+		for (size_t i = 0; i < segments + 1; i++)
+		{
+			vertices.push_back(vertices[5 * i]);
+			vertices.push_back(vertices[5 * i + 1]);
+			vertices.push_back(vertices[5 * i + 2] + height);
+			vertices.push_back(vertices[5 * i + 3] + 0.5f);
+			vertices.push_back(vertices[5 * i + 4]);
+		}
+
+		// Vertices of lateral surface
+		for (size_t i = 0; i < stacks; i++)
+		{
+			GLfloat dh = (float)i / (float)(stacks - 1) * height;
+			for (size_t j = 0; j < segments; j++)
+			{
+				vertices.push_back(vertices[5 * (j + 1)]);
+				vertices.push_back(vertices[5 * (j + 1) + 1]);
+				vertices.push_back(vertices[5 * (j + 1) + 2] + dh);
+				vertices.push_back((float)j / (float)segments);
+				vertices.push_back(0.5f + (float)i / (float)(2 * (stacks - 1)));
 			}
+			vertices.push_back(vertices[5]);
+			vertices.push_back(vertices[6]);
+			vertices.push_back(vertices[7] + dh);
+			vertices.push_back(1.0f);
+			vertices.push_back(0.5f + (float)i / (float)(2 * (stacks - 1)));
 		}
 	}
 
@@ -87,48 +116,54 @@ private:
 	{
 
 		/*
-		Adding indices for triangles in Top and Bottom planes (and also between them if stacks > 2)
+		Adding indices for triangles in Top and Bottom planes
 		*/
 		GLuint centerOfPlaneIndex;
-		for (size_t i = 0; i < stacks; i++)
-		{
-			centerOfPlaneIndex = i * (segments + 1);
-			indices.push_back(centerOfPlaneIndex);
-			indices.push_back(centerOfPlaneIndex + segments);
-			indices.push_back(centerOfPlaneIndex + 1);
+		
+		centerOfPlaneIndex = 0;
+		indices.push_back(centerOfPlaneIndex);
+		indices.push_back(centerOfPlaneIndex + segments);
+		indices.push_back(centerOfPlaneIndex + 1);
 
-			for (size_t j = 1; j <= segments - 1; j++)
-			{
-				indices.push_back(centerOfPlaneIndex);
-				indices.push_back(centerOfPlaneIndex + j);
-				indices.push_back(centerOfPlaneIndex + j + 1);
-			}
+		for (size_t j = 1; j <= segments - 1; j++)
+		{
+			indices.push_back(centerOfPlaneIndex);
+			indices.push_back(centerOfPlaneIndex + j);
+			indices.push_back(centerOfPlaneIndex + j + 1);
 		}
+
+		centerOfPlaneIndex = segments + 1;
+		indices.push_back(centerOfPlaneIndex);
+		indices.push_back(centerOfPlaneIndex + segments);
+		indices.push_back(centerOfPlaneIndex + 1);
+
+		for (size_t j = 1; j <= segments - 1; j++)
+		{
+			indices.push_back(centerOfPlaneIndex);
+			indices.push_back(centerOfPlaneIndex + j);
+			indices.push_back(centerOfPlaneIndex + j + 1);
+		}
+		
 
 		/*
 		Adding indices for triangles between planes
 		*/
-		GLuint centerIndexLowerPlane;
-		GLuint centerIndexHigherPlane;
+		GLuint lowerPlaneFirstIndex;
+		GLuint higherPlaneFirstIndex;
 		for (size_t i = 0; i < stacks - 1; i++)
 		{
-			centerIndexLowerPlane = i * (segments + 1);
-			centerIndexHigherPlane = (i + 1) * (segments + 1);
+			lowerPlaneFirstIndex = (i + 2) * (segments + 1);
+			higherPlaneFirstIndex = (i + 3) * (segments + 1);
 
-			indices.push_back(centerIndexLowerPlane + segments);
-			indices.push_back(centerIndexHigherPlane + segments);
-			indices.push_back(centerIndexHigherPlane + 1);
-			indices.push_back(centerIndexHigherPlane + 1);
-			indices.push_back(centerIndexLowerPlane + segments);
-			indices.push_back(centerIndexLowerPlane + 1);
-			for (size_t j = 1; j <= segments - 1; j++)
+			for (size_t j = 0; j < segments; j++)
 			{
-				indices.push_back(centerIndexLowerPlane + j);
-				indices.push_back(centerIndexHigherPlane + j);
-				indices.push_back(centerIndexHigherPlane + j + 1);
-				indices.push_back(centerIndexHigherPlane + j + 1);
-				indices.push_back(centerIndexLowerPlane + j);
-				indices.push_back(centerIndexLowerPlane + j + 1);
+				indices.push_back(lowerPlaneFirstIndex + j);
+				indices.push_back(higherPlaneFirstIndex + j);
+				indices.push_back(higherPlaneFirstIndex + j + 1);
+
+				indices.push_back(higherPlaneFirstIndex + j + 1);
+				indices.push_back(lowerPlaneFirstIndex + j);
+				indices.push_back(lowerPlaneFirstIndex + j + 1);
 			}
 		}
 	}
@@ -147,13 +182,44 @@ private:
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), &indices[0], GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 
 		glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
 	
+		// Set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// Set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// prepare textures
+		int width, height;
+		unsigned char* image = SOIL_load_image("textures/woodBarrel.png", &width, &height, 0, SOIL_LOAD_RGB);
+		if (image == nullptr)
+			throw std::exception("Failed to load texture file");
+
+		
+		glGenTextures(1, &texture0);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture0);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		// freeing unnecessary texture stuff
+		SOIL_free_image_data(image);
+		shader->use(); // don't forget to activate/use the shader before setting uniforms!
+	// either set it manually like so:
+		glUniform1i(glGetUniformLocation(shader->ID, "Texture0"), 0);
+		// or set it via the texture class
+		glBindTexture(GL_TEXTURE_2D, 0);
+
 	}
 
 	/*
@@ -161,8 +227,8 @@ private:
 	*/
 	void init()
 	{
-		vertices.reserve(2 * (segments + 1));
-		indices.reserve(4 * segments);
+		vertices.reserve((segments + 1) * (stacks + 2));
+		indices.reserve(6 * segments * stacks);
 		shader = ShaderProvider::instance().getShader("cylinderSh.vert", "cylinderSh.frag");
 		generateVertices();
 		generateIndices();
@@ -172,17 +238,17 @@ private:
 	
 
 public:
-	// template solution, shouldnt be in pushed version
+	// template solution
 	static std::shared_ptr<Shader> getShaderPtr()
 	{
-		return ShaderProvider::instance().getShader("cylinderSh.vert", "cylinderSh.frag");
+		return ShaderProvider::instance().getShader("shCylinder.vert", "shCylinder.frag");
 	}
 
 	/*
 	Default constructor with fixed parameters
 	*/
 	Cylinder() :
-		coordinates(NULL_VECTOR),
+		coordinates(glm::vec3(0.0f, 0.0f, 0.0f)),
 		height(1.0),
 		radius(1.0),
 		segments(DEFAULT_SEGMENTS_NUMBER),
@@ -234,18 +300,17 @@ public:
 
 	virtual void draw()
 	{
-		shader->use();
-		float radius = 10.0f;
-		float camX = sin(glfwGetTime()) * radius;
-		float camZ = cos(glfwGetTime()) * radius;
-		glm::mat4 view = glm::mat4(1.0f);;
-		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
-		shader->setTransformMatrix("view", view);
 		glm::mat4 model = glm::mat4(1.0f);
 		model = glm::translate(model, coordinates);
-		model = glm::rotate(model, glm::radians(xRotation), glm::vec3(1.0f, 0.0f, 0.0f));
-		model = glm::rotate(model, glm::radians(yRotation), glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::rotate(model, glm::radians(zRotation), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::rotate(model, glm::radians(this->xRotation), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(this->yRotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(this->zRotation), glm::vec3(0.0f, 0.0f, 1.0f));
+		
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture0);
+		shader->use();
+		glUniform1i(glGetUniformLocation(shader->ID, "Texture0"), 0);
 		shader->setTransformMatrix("model", model);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
@@ -259,11 +324,9 @@ public:
 
 	void rotate(GLfloat xRotation, GLfloat yRotation, GLfloat zRotation)
 	{
-		// todo: Add translation move to 0,0,0 first then rotate then translation back
-		this->xRotation = xRotation;
-		this->yRotation = yRotation;
-		this->zRotation = zRotation;
-
+		this->xRotation += xRotation;
+		this->yRotation += yRotation;
+		this->zRotation += zRotation;
 	}
 
 	/*
@@ -272,17 +335,19 @@ public:
 	void print()
 	{
 		std::cout << "Vertices" << std::endl;
-		for (size_t i = 0; i < vertices.size()/3; i++)
+		for (size_t i = 0; i < vertices.size()/5; i++)
 		{
-			std::cout << vertices[3 * i] << ' ';
-			std::cout << vertices[3 * i + 1] << ' ';
-			std::cout << vertices[3 * i + 2] << std::endl;
+			std::cout << i << ") "<< vertices[5 * i] << ' ';
+			std::cout << vertices[5 * i + 1] << ' ';
+			std::cout << vertices[5 * i + 2] << ' ';
+			std::cout << vertices[5 * i + 3] << ' ';
+			std::cout << vertices[5 * i + 4] << std::endl;
 		}
 		std::cout << "Indices" << std::endl;
 		for (size_t i = 0; i < indices.size() / 3; i++)
 		{
-			std::cout << 3 * i << ") " << indices[3 * i] << "\t";
-			std::cout << 3 * i + 1 << ") " << indices[3 * i + 1] << "\t";
+			std::cout << 3 * i << ") " << indices[3 * i] << ' ';
+			std::cout << 3 * i + 1 << ") " << indices[3 * i + 1] << ' ';
 			std::cout << 3 * i + 2 << ") " << indices[3 * i + 2] << std::endl;
 		}
 	}
