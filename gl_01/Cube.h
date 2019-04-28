c4019572499783ad0deb63a55beabe2709bd4b85
rlewancz <rlewancz@mion.elka.pw.edu.pl>
2019-04-28 15:16:51 +0200

    corrected textures and translations

--- gl_01/Cube.h
+++ gl_01/Cube.h
@@ -8,6 +8,7 @@
 #include "ShaderProvider.h"
 #include "Shader.h"
 #include "SOIL.h"
+#include "Texture.h"
 #include <math.h>
 
 class Cube : Object
@@ -22,7 +23,7 @@ private:
 	const std::string texturePath; 
 	std::vector <GLfloat> V;
 	std::vector <GLuint> indices; 
-
+	Texture texture; 
 
 	void setUpBuffers()
 	{
@@ -47,35 +48,7 @@ private:
 		glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 
 		glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
-
-		// Set the texture wrapping parameters
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		// Set texture filtering parameters
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		// prepare textures
-		int width, height;
-		unsigned char* image = SOIL_load_image(texturePath.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
-		if (image == nullptr)
-			throw std::exception("Failed to load texture file");
-
-
-		glGenTextures(1, &texture0);
-
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture0);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-		glGenerateMipmap(GL_TEXTURE_2D);
-		// freeing unnecessary texture stuff
-		SOIL_free_image_data(image);
-		shader->use(); // don't forget to activate/use the shader before setting uniforms!
-		// either set it manually like so:
-		glUniform1i(glGetUniformLocation(shader->ID, "Texture0"), 0);
-		// or set it via the texture class
-		glBindTexture(GL_TEXTURE_2D, 0);
-
+		texture = Texture(texturePath); 
 	}
 	void insertVert(glm::vec3 v)
 	{
@@ -180,17 +153,14 @@ public :
 	virtual void draw()
 	{
 		glm::mat4 model = glm::mat4(1.0f);
-		
+		model = glm::translate(model, coordinates);
 		model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
 		model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
 		model = glm::scale(model, size); 
-		model = glm::translate(model, coordinates);
+		
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture0);
-		shader->use();
-		glUniform1i(glGetUniformLocation(shader->ID, "Texture0"), 0);
+		texture.useTexture(shader); 
 		shader->setTransformMatrix("model", model);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
