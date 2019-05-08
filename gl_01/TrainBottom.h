c5e333bcae8b106ca6352c9257c826af09d2946a
Roman Moskalenko <rmoskale@mion.elka.pw.edu.pl>
2019-05-09 00:20:53 +0200

    Basic displaying done, added movement

---
+++ gl_01/TrainBottom.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "Composite.h"
+#include "Sphere.h"
+#include "Cylinder.h"
+#include "Cube.h"
+
+
+class Rods : public Composite
+{
+private:
+	const std::string rodTexturePath = "textures/metal.jpg";
+	const std::string circleTexturePath = "textures/steelWheelSmall.png";
+
+public:
+	Rods()
+	{
+		auto rodLeft = std::make_unique<Cube>(glm::vec3(3.0f, 0.0f, -0.35f - 0.01f), glm::vec3(3.2f, 0.2f, 0.1f), rodTexturePath);
+		auto rodRight = std::make_unique<Cube>(glm::vec3(3.0f, 0.0f, 4.35f + 0.01f), glm::vec3(3.2f, 0.2f, 0.1f), rodTexturePath);
+		auto leftCircleOfLeftRod = std::make_unique<Cylinder>(glm::vec3(6.0f, 0.0f, -0.55f - 0.01f), 0.3f, 0.34f, circleTexturePath);
+		auto rightCircleOfLeftRod = std::make_unique<Cylinder>(glm::vec3(0.0f, 0.0f, -0.55f - 0.01f), 0.3f, 0.34f, circleTexturePath);
+		auto leftCircleOfRightRod = std::make_unique<Cylinder>(glm::vec3(6.0f, 0.0f, 4.25f + 0.01f), 0.3f, 0.34f, circleTexturePath);
+		auto rightCircleOfRightRod = std::make_unique<Cylinder>(glm::vec3(0.0f, 0.0f, 4.25f + 0.01f), 0.3f, 0.34f, circleTexturePath);
+
+		addChild(std::move(rodLeft));
+		addChild(std::move(rodRight));
+		addChild(std::move(leftCircleOfLeftRod));
+		addChild(std::move(rightCircleOfLeftRod));
+		addChild(std::move(leftCircleOfRightRod));
+		addChild(std::move(rightCircleOfRightRod));
+
+	}
+};
+
+class Wheels : public Composite
+{
+public:
+	Wheels()
+	{
+		auto wheelFrontLeft = std::make_unique<Cylinder>(glm::vec3(0.0f, 0.0f, -0.25f - 0.01f), 0.25f, 1.35f, "textures/steelWheelSmall.png");
+		auto wheelBackLeft = std::make_unique<Cylinder>(glm::vec3(6.0f, 0.0f, -0.25f - 0.01f), 0.25f, 1.35f, "textures/steelWheelSmall.png");
+		auto wheelFrontRight = std::make_unique<Cylinder>(glm::vec3(0.0f, 0.0f, 4.0f + 0.01f), 0.25f, 1.35f, "textures/steelWheelSmall.png");
+		auto wheelBackRight = std::make_unique<Cylinder>(glm::vec3(6.0f, 0.0f, 4.0f + 0.01f), 0.25f, 1.35f, "textures/steelWheelSmall.png");
+		addChild(std::move(wheelFrontLeft));
+		addChild(std::move(wheelFrontRight));
+		addChild(std::move(wheelBackLeft));
+		addChild(std::move(wheelBackRight));
+	}
+};
+
+class TrainBottom : public Composite
+{
+public:
+	Wheels * wheelsPtr;
+	Rods * rodsPtr;
+	TrainBottom()
+	{
+		auto wheels = std::make_unique<Wheels>();
+		wheelsPtr = wheels.get();
+		addChild(std::move(wheels));
+		auto rods = std::make_unique<Rods>();
+		rodsPtr = rods.get();
+		addChild(std::move(rods));
+		auto bottom = std::make_unique<Cube>(glm::vec3(3.0f, 0.25f, 2.0f), glm::vec3(7.0f, 0.5, 2.0f), "textures/container.jpg");
+		addChild(std::move(bottom));
+	}
+};
+
+
--- gl_01/gl_01.cpp
+++ gl_01/gl_01.cpp
@@ -2,6 +2,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 #include "Camera.h"
 #include "Cylinder.h"
@@ -9,20 +11,23 @@
 #include "Cube.h"
 #include "Sphere.h"
 #include "ThreeShapes.h"
+#include "TrainBottom.h"
 #include "Text.h"
+
 
 using namespace std;
 
 typedef std::vector<std::shared_ptr<Shader>>  ShaderVector;
 
 const GLuint SCR_WIDTH = 1600;
-const GLuint SCR_HEIGHT = 1200;
+const GLuint SCR_HEIGHT = 1000;
 
 void applyViewToShaders(ShaderVector shaders, const glm::mat4& projection, const glm::mat4& view);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+string doubleToString(double);
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 double lastX;
@@ -32,6 +37,8 @@ bool firstMouse = true;
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
+float angle = 0.0f;
+float pos = 0.0f;
 
 int main()
 {
@@ -57,7 +64,7 @@ int main()
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		// Set OpenGL options
 		//glEnable(GL_CULL_FACE);
-		
+
 		glfwMakeContextCurrent(window);
 		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 		glfwSetCursorPosCallback(window, mouse_callback);
@@ -73,38 +80,71 @@ int main()
 		auto shSphere = Sphere::getShaderPtr();
 		auto shCube = Cube::getShaderPtr();
 
-		auto threeShapes = ThreeShapes();
+		auto trainBottom = TrainBottom();
 
 		glEnable(GL_DEPTH_TEST);
 		glDepthFunc(GL_LESS);
 
-		//cylinder->print();
+		double lastTime = lastFrame = glfwGetTime(), deltaT = 0.0, spf = 0.0;
+		int nbFrames = 0;
+		float dx0 = 0.0f, dy0 = 0.0f;
+
 		// main loop
 		while (!glfwWindowShouldClose(window))
 		{
 			float currentFrame = glfwGetTime();
 			deltaTime = currentFrame - lastFrame;
+			deltaT = currentFrame - lastTime;
 			lastFrame = currentFrame;
-
+			nbFrames++;
+			// input
+			// -----
 			processInput(window);
 
 			mouse_callback(window, lastX, lastY);
+			//scroll_callback(window, 3, 3);
+			// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 
 
+			// Clear the colorbuffer
 			glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+			//glClear(GL_COLOR_BUFFER_BIT);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			RenderText(shText, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(1, 1, 1));
-			
+			if (deltaT >= 0.25) { // If last prinf() was more than 1 sec ago
+		 // printf and reset timer
+				spf = 250 / (double)nbFrames;
+				nbFrames = 0;
+				lastTime += 0.25f;
+			}
+
+			RenderText(shText, "frame: " + doubleToString(spf) + "ms", 25.0f, SCR_HEIGHT - 20.0f, 0.4f, glm::vec3(1.0f));
+			RenderText(shText, "FPS: " + std::to_string((int)(1000 / spf)), 25.0f, SCR_HEIGHT - 50.0f, 0.4f, glm::vec3(1.0f));
+			RenderText(shText, "X=" + doubleToString(camera.Position.x) + "; Y=" + doubleToString(camera.Position.y) + "; Z=" + doubleToString(camera.Position.z), 25.0f, SCR_HEIGHT - 80.0f, 0.4f, glm::vec3(1.0f));
+
+
+
 			auto projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 			auto view = camera.GetViewMatrix();
+
 			applyViewToShaders({ shCylinder, shCube, shSphere }, projection, view);
 
-			threeShapes.move({ 0.001f, 0.0f, 0.0f });
-			threeShapes.rotate({ 0.1f, 0.0f, 0.0f });
-			threeShapes.draw();
+			// 0.447 - distance from wheel center to rod; it's 0.3311 * wheel_radius
+			// 150 - angle offset in degrees
+			float dx = 0.447 * cos((pos-150)/180*M_PI);
+			float dy = 0.447 * sin((pos-150)/180*M_PI);
 
+			trainBottom.draw();
+			trainBottom.wheelsPtr->rotate({0.0f, 0.0f, deltaTime * angle});
+			trainBottom.rodsPtr->move({ -dx0+dx, -dy0+dy, 0.0f });
+			dx0 = dx;
+			dy0 = dy;
+			pos += deltaTime*angle;
+			// 1.35 - radius of wheels
+			trainBottom.move({ -deltaTime * angle / 180 * M_PI * 1.35f, 0.0f, 0.0f });
 			
+
+
 
 			// Swap the screen buffers
 			glfwSwapBuffers(window);
@@ -151,6 +191,10 @@ void processInput(GLFWwindow *window)
 		camera.ProcessKeyboard(LEFT, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		angle += 0.5f;
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		angle -= 0.5f;
 }
 
 
@@ -179,4 +223,17 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	camera.ProcessMouseScroll(yoffset);
+}
+
+string doubleToString(double var)
+{
+	// Create an output string stream
+	std::ostringstream streamObj;
+	// Set Fixed -Point Notation
+	streamObj << std::fixed;
+	// Set precision to 2 digits
+	streamObj << std::setprecision(2);
+	//Add double to stream
+	streamObj << var;
+	return streamObj.str().c_str();
 }
