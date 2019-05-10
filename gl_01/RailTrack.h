7f5f1e3991b5fd7533b748262b2d98cdc72412e8
Rafał <rlewancz@mion.elka.pw.edu.pl>
2019-05-10 15:59:02 +0200

    removed arrays

--- gl_01/RailTrack.h
+++ gl_01/RailTrack.h
@@ -8,18 +8,18 @@ class RailTrack : public Composite
 public: 
 	RailTrack(int number, float offset)
 	{
-		std::unique_ptr<Cube> *rail1 = new std::unique_ptr<Cube> [number];
-		std::unique_ptr<Cube> *rail2 = new std::unique_ptr<Cube> [number]; 
-		std::unique_ptr<Cube> *wood = new std::unique_ptr<Cube>[number];
+		std::unique_ptr<Cube> rail1;
+		std::unique_ptr<Cube> rail2; 
+		std::unique_ptr<Cube> wood;
 		for (int i = 0; i < number; i++)
 		{
-			rail1[i] = std::make_unique<Cube>(glm::vec3(0.2f, 0.0f, 0.0f + offset * i), glm::vec3(0.05f, 0.05f, 0.3f), "textures/metal.jpg");
-			rail2[i] = std::make_unique<Cube>(glm::vec3(-0.2f, 0.0f, 0.0f + offset *i), glm::vec3(0.05f, 0.05f, 0.3f), "textures/metal.jpg");
-			wood[i] = std::make_unique<Cube>(glm::vec3(0.0f, 0.0f, 0.0f+ offset * i), glm::vec3(0.3f, 0.02f, 0.1f), "textures/oldWood.jpg"); 
+			rail1 = std::make_unique<Cube>(glm::vec3(0.2f, 0.0f, 0.0f + offset * i), glm::vec3(0.05f, 0.05f, 0.3f), "textures/metal.jpg");
+			rail2 = std::make_unique<Cube>(glm::vec3(-0.2f, 0.0f, 0.0f + offset *i), glm::vec3(0.05f, 0.05f, 0.3f), "textures/metal.jpg");
+			wood = std::make_unique<Cube>(glm::vec3(0.0f, 0.0f, 0.0f+ offset * i), glm::vec3(0.3f, 0.02f, 0.1f), "textures/oldWood.jpg"); 
 
-			addChild(std::move(rail1[i])); 
-			addChild(std::move(rail2[i]));
-			addChild(std::move(wood[i]));
+			addChild(std::move(rail1)); 
+			addChild(std::move(rail2));
+			addChild(std::move(wood));
 		}
 			
 	}
