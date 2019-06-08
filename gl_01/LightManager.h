99b902198bc37f2beadf69945d79f5b2d83338e0
Roman Moskalenko <rmoskale@mion.elka.pw.edu.pl>
2019-06-08 23:53:19 +0200

    Updated

--- gl_01/LightManager.h
+++ gl_01/LightManager.h
@@ -339,7 +339,7 @@ public:
 	void setDirLight()
 	{
 		setDirLight({1.0f, -0.62f, 0.175f},
-					{0.2f, 0.2f, 0.2f},
+					{0.35f, 0.35f, 0.35f},
 					{0.5f, 0.5f, 0.5f},
 					{0.3f, 0.3f, 0.3f});
 	}
