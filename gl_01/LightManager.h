0624b5a4922a5db2e930647c1a17c9c02307f2a9
Roman Moskalenko <rmoskale@mion.elka.pw.edu.pl>
2019-06-09 11:53:14 +0200

    Removed commented code

--- gl_01/LightManager.h
+++ gl_01/LightManager.h
@@ -170,157 +170,9 @@ private:
 		}
 	};
 
-	/*class SpotLight {
-	private:
-		glm::vec3 position;
-		glm::vec3 direction;
-		GLfloat cutOff;
-		GLfloat outerCutOff;
-		glm::vec3 ambient;
-		glm::vec3 diffuse;
-		glm::vec3 specular;
-		GLfloat constant;
-		GLfloat linear;
-		GLfloat quadratic;
-		std::unique_ptr<LightSrc> lightsrc;
-
-	public:
-		SpotLight(const glm::vec3& position,
-			const glm::vec3& direction,
-			const GLfloat& cutOff,
-			const GLfloat& outerCutOff,
-			const glm::vec3& ambient,
-			const glm::vec3& diffuse,
-			const glm::vec3& specular,
-			const GLfloat& constant,
-			const GLfloat& linear,
-			const GLfloat& quadratic)
-			: position(position),
-			direction(direction),
-			cutOff(cutOff),
-			outerCutOff(outerCutOff),
-			ambient(ambient),
-			diffuse(diffuse),
-			specular(specular),
-			constant(constant),
-			linear(linear),
-			quadratic(quadratic),
-			lightsrc(std::make_unique<LightSrc>(position, 0.5f))
-		{}
-			
-
-		glm::vec3 getPosition()
-		{
-			return position;
-		}
-
-		glm::vec3 getDirection()
-		{
-			return direction;
-		}
-
-		GLfloat getCutOff()
-		{
-			return cutOff;
-		}
-
-		GLfloat getOuterCutOff()
-		{
-			return outerCutOff;
-		}
-
-		glm::vec3 getAmbient()
-		{
-			return ambient;
-		}
-
-		glm::vec3 getDiffuse()
-		{
-			return diffuse;
-		}
-
-		glm::vec3 getSpecular()
-		{
-			return specular;
-		}
-
-		GLfloat getConstant()
-		{
-			return constant;
-		}
-
-		GLfloat getLinear()
-		{
-			return linear;
-		}
-
-		GLfloat getQuadratic()
-		{
-			return quadratic;
-		}
-
-		void setPosition(const glm::vec3& newPosition)
-		{
-			lightsrc->move(newPosition - position);
-			position = newPosition;
-		}
-
-		void setDirection(const glm::vec3& newDirection)
-		{
-			direction = newDirection;
-		}
-
-		void setCutOff(const GLfloat& newCutOff)
-		{
-			cutOff = newCutOff;
-		}
-
-		void setOuterCutOff(const GLfloat& newOuterCutOff)
-		{
-			outerCutOff = newOuterCutOff;
-		}
-
-		void setAmbient(const glm::vec3& newAmbient)
-		{
-			ambient = newAmbient;
-		}
-
-		void setDiffuse(const glm::vec3& newDiffuse)
-		{
-			diffuse = newDiffuse;
-		}
-
-		void setSpecular(const glm::vec3& newSpecular)
-		{
-			specular = newSpecular;
-		}
-
-		void setConstant(const GLfloat& newConstant)
-		{
-			constant = newConstant;
-		}
-
-		void setLinear(const GLfloat& newLinear)
-		{
-			linear = newLinear;
-		}
-
-		void setQuadratic(const GLfloat& newQuadratic)
-		{
-			quadratic = newQuadratic;
-		}
-
-		void drawLightSrc()
-		{
-			lightsrc->draw();
-		}
-
-	};*/
-
 	std::shared_ptr<Shader> lightSrcShader;
 	std::unique_ptr<DirectionalLight> dirLight;
 	std::shared_ptr<PointLight> pointLight;
-	//std::shared_ptr<SpotLight> spotLight;
 	
 	Camera camera;
 	
@@ -330,11 +182,6 @@ public:
 		this->camera = camera;
 		dirLight = std::make_unique<DirectionalLight>(DirectionalLight());
 	}
-
-	/*void setShader(std::shared_ptr<Shader> shader)
-	{
-		this->shader = shader;
-	}*/
 
 	void setDirLight()
 	{
@@ -398,99 +245,9 @@ public:
 		return pointLight;
 	}
 
-	/*void addNewPointLight()
-	{
-		addNewPointLight({ 0.0f, 0.0f, 0.0f },
-						{ 0.05f, 0.05f, 0.05f },
-						{ 0.5f, 0.5f, 0.5f },
-						{ 0.3f, 0.3f, 0.3f },
-						1.0f,
-						0.09f,
-						0.032f);
-	}
-
-	void addNewPointLight(const glm::vec3& position,
-						const glm::vec3& ambient,
-						const glm::vec3& diffuse,
-						const glm::vec3& specular,
-						const GLfloat& constant,
-						const GLfloat& linear,
-						const GLfloat& quadratic)
-	{
-		if (pointLights.size() < 4)
-			pointLights.push_back(std::make_unique<PointLight>(PointLight(position,
-											ambient,
-											diffuse,
-											specular,
-											constant,
-											linear,
-											quadratic)));
-
-	}*/
-
-	/*void popLastPointLight()
-	{
-		if (pointLights.size() > 0)
-			pointLights.pop_back();
-	}*/
-
-	/*void movePointLight(unsigned int index, glm::vec3 displacement)
-	{
-		if (index >= pointLights.size())
-			return;
-		pointLights[index]->setPosition(pointLights[index]->getPosition() + displacement);
-	}*/
-
-	/*void moveSpotLight(glm::vec3 displacement)
-	{
-		if (!spotLight)
-			return;
-		spotLight->setPosition(spotLight->getPosition() + displacement);
-	}
-
-	std::shared_ptr<SpotLight> getSpotLight()
-	{
-		return getSpotLight({-3.0f, 2.0f, 2.0f},
-							{ -1.0f, 0.0f, 0.0f },
-							glm::cos(glm::radians(30.0f)),
-							glm::cos(glm::radians(45.0f)),
-							glm::vec3(0.2f),
-							glm::vec3(1.0f),
-							glm::vec3(0.8f),
-							1.0f,
-							0.09f,
-							0.032f);
-	}*/
-
-	/*std::shared_ptr<SpotLight> getSpotLight(const glm::vec3& position,
-											const glm::vec3& direction,
-											const GLfloat& cutOff,
-											const GLfloat& outerCutOff,
-											const glm::vec3& ambient,
-											const glm::vec3& diffuse,
-											const glm::vec3& specular,
-											const GLfloat& constant,
-											const GLfloat& linear,
-											const GLfloat& quadratic)
-	{
-		spotLight = std::make_shared<SpotLight>(SpotLight(position,
-														direction,
-														cutOff,
-														outerCutOff,
-														ambient,
-														diffuse,
-														specular,
-														constant,
-														linear,
-														quadratic));
-		return spotLight;
-	}*/
-
-
 	void applyLightToShader(std::shared_ptr<Shader> shader)
 	{
 		shader->use();
-		//shader->setInt("nr_point_lights", pointLights.size());
 		shader->setVec3f("viewPos", camera.Position);
 		shader->setVec3f("dirLight.direction", dirLight->getDirection());
 		shader->setVec3f("dirLight.ambient", dirLight->getAmbient() + 0.15f );
@@ -506,42 +263,7 @@ public:
 		shader->setFloat("pointLight.quadratic", pointLight->getQuadratic());
 		pointLight->drawLightSrc(this->lightSrcShader);
 
-		/*for (size_t i = 0; i < pointLights.size(); i++)
-		{
-			//pointLights[i]->drawLightSrc();
-			shader->use();
-			shader->setVec3f("pointLight.position", pointLight->getPosition());
-			shader->setVec3f("pointLight.ambient", pointLight->getAmbient());
-			shader->setVec3f("pointLight.diffuse", pointLight->getDiffuse());
-			shader->setVec3f("pointLight.specular", pointLight->getSpecular());
-			shader->setFloat("pointLight.constant", pointLight->getConstant());
-			shader->setFloat("pointLight.linear", pointLight->getLinear());
-			shader->setFloat("pointLight.quadratic", pointLight->getQuadratic());
-		}
-
-		if (spotLight)
-		{
-			spotLight->drawLightSrc();
-			shader->use();
-			shader->setVec3f("spotLight.position", spotLight->getPosition());
-			shader->setVec3f("spotLight.direction", spotLight->getDirection());
-			shader->setFloat("spotLight.cutOff", spotLight->getCutOff());
-			shader->setFloat("spotLight.outerCutOff", spotLight->getOuterCutOff());
-			shader->setFloat("spotLight.constant", spotLight->getConstant());
-			shader->setFloat("spotLight.linear", spotLight->getLinear());
-			shader->setFloat("spotLight.quadratic", spotLight->getQuadratic());
-			shader->setVec3f("spotLight.ambient", spotLight->getAmbient());
-			shader->setVec3f("spotLight.diffuse", spotLight->getDiffuse());
-			shader->setVec3f("spotLight.specular", spotLight->getSpecular());
-
-		}*/
-
 	}
-
-	/*int getLightSrcsCount()
-	{
-		return pointLights.size();
-	}*/
 
 
 };
--- gl_01/shCube.frag
+++ gl_01/shCube.frag
@@ -22,21 +22,6 @@ struct PointLight {
     vec3 specular;
 };
 
-//struct SpotLight {
-//    vec3 position;
-//    vec3 direction;
-//    float cutOff;
-//    float outerCutOff;
-//  
-//    float constant;
-//    float linear;
-//    float quadratic;
-//  
-//    vec3 ambient;
-//    vec3 diffuse;
-//    vec3 specular;       
-//};
-
 in vec2 TexCoords;
 in vec3 Normal;
 in vec3 FragPos;
@@ -49,46 +34,21 @@ uniform vec3 lightPos;
 uniform vec3 viewPos;
 uniform DirLight dirLight;
 uniform PointLight pointLight;
-//uniform SpotLight spotLight;
 
 // function prototypes
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 float ShadowCalculation(vec4 fragPosLightSpace);
-//vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
 
 void main()
 {
-//    vec4 texColor = texture(Texture0, TexCoord); 
-//	if(texColor.a < 0.1)
-//        discard;
-//
-//	// ambient
-//    float ambientStrength = 0.5f;
-//    vec3 ambient = ambientStrength * lightColor;
-//
-//	// diffuse 
-//    vec3 norm = normalize(Normal);
-//    vec3 lightDir = normalize(lightPos - FragPos);
-//    float diff = max(dot(norm, lightDir), 0.0);
-//    vec3 diffuse = diff * lightColor;
-//
-//	// specular
-//    float specularStrength = 0.5;
-//    vec3 viewDir = normalize(viewPos - FragPos);
-//    vec3 reflectDir = reflect(-lightDir, norm);  
-//    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-//    vec3 specular = specularStrength * spec * lightColor;  
-//	vec3 result = (ambient + diffuse + specular) * vec3(texColor);
-//    color = vec4(result, 1.0);
-
 	// properties
     vec3 norm = normalize(Normal);
     vec3 viewDir = normalize(viewPos - FragPos);
     
     // == =====================================================
-    // Our lighting is set up in 3 phases: directional, point lights and an optional flashlight
+    // Our lighting is set up in 3 phases: directional, point lights and an optional flashlight (no flashlight)
     // For each phase, a calculate function is defined that calculates the corresponding color
     // per lamp. In the main() function we take all the calculated colors and sum them up for
     // this fragment's final color.
@@ -98,8 +58,7 @@ void main()
 //    // phase 2: point lights
     
 	result += CalcPointLight(pointLight, norm, FragPos, viewDir);    
-//    // phase 3: spot light
-//    result += CalcSpotLight(spotLight, norm, FragPos, viewDir);    
+   
     
     color = vec4(result, 1.0);
 }
@@ -176,30 +135,3 @@ float ShadowCalculation(vec4 fragPosLightSpace)
         
     return shadow;
 }
-
-
-//// calculates the color when using a spot light.
-//vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
-//{
-//    vec3 lightDir = normalize(light.position - fragPos);
-//    // diffuse shading
-//    float diff = max(dot(normal, lightDir), 0.0);
-//    // specular shading
-//    vec3 reflectDir = reflect(-lightDir, normal);
-//    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-//    // attenuation
-//    float distance = length(light.position - fragPos);
-//    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
-//    // spotlight intensity
-//    float theta = dot(lightDir, normalize(-light.direction)); 
-//    float epsilon = light.cutOff - light.outerCutOff;
-//    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
-//    // combine results
-//    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
-//    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
-//    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
-//    ambient *= attenuation * intensity;
-//    diffuse *= attenuation * intensity;
-//    specular *= attenuation * intensity;
-//    return (ambient + diffuse + specular);
-//}
